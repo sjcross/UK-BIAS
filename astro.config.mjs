d56c24f3103d5525cb80065db01018c2c3a431e4
Stephen Cross <stephencross86@gmail.com>
2026-07-13 11:24:28 +0100

    Updated base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sjcross.github.io',
-  base: '/UK-BIAS/',
+  base: '/UK-BIAS',
   vite: {
     plugins: [tailwindcss()]
   }
