017436c1d645bb3fe60e1d0e0ab89b959fc20e90
Stephen Cross <stephencross86@gmail.com>
2026-07-13 11:11:47 +0100

    Updated Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import solidJs from '@astrojs/solid-js';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
   site: 'https://sjcross.github.io',
-  base: '/UK-BIAS',
-
+  base: '/UK-BIAS/',
   vite: {
     plugins: [tailwindcss()]
   }
